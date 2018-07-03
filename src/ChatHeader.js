a5a744168a7989c3fb3654c06d50a1c221a78388
Jayden Thrasher <jayden_thrasher@yahoo.com>
2018-07-03 10:14:25 -0400

    Convert ChatHeader to stateless function

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ChatHeader extends Component {
-  render() {
-    return (
-      <header className="chatHeader">
-        <dive className="roomInfo">
-          <h2>#general</h2>
-          <p>Announcements and general chat</p>
-        </dive>
-      </header>
-    );
-  }
-}
+const ChatHeader = () => {
+  return (
+    <header className="chatHeader">
+      <div className="roomInfo">
+        <h2>#general</h2>
+        <p>Announcements and general chat</p>
+      </div>
+    </header>
+  );
+};
+
 
 export default ChatHeader;
