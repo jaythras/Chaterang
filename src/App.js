58ef1fc5a96937a0f3c18474067e63d7b60d884b
Jayden Thrasher <jayden_thrasher@yahoo.com>
2018-07-07 14:55:02 -0400

    Add SignIn component and functionality

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Main from './Main';
+import SignIn from './SignIn';
 
 class App extends Component {
   constructor() {
     super();
 
+    const user = JSON.parse(localStorage.getItem('user'));
+
     this.state = {
-      user: {
-        uid: 'adsflkadsjfaslkdfj',
-        displayName: 'Jayden',
-        email: 'jayden@gmail.com',
-      }
-    }
+      user: user || {},
+    };
   }
+
+  handleAuth = (user) => {
+    this.setState({ user });
+    localStorage.setItem('user', JSON.stringify(user));
+  };
+
+  signedIn = () => {
+    return this.state.user.uid;
+  };
+
+
   render() {
     return (
       <div className="App">
-        <Main user={this.state.user} />
+        {
+          this.signedIn()
+            ?
+            <Main
+              user={this.state.user}
+
+            />
+            : <SignIn handleAuth={this.handleAuth} />
+        }
+
       </div>
     );
   }
