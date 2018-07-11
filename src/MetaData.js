b318ea8c50d4936ff9debf77070f678e9e047144
Jayden Thrasher <jayden_thrasher@yahoo.com>
2018-07-11 09:47:51 -0400

    Use moment JS to show time

--- src/MetaData.js
+++ src/MetaData.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 const Metadata = ({ message }) => {
   return (
@@ -10,7 +11,7 @@ const Metadata = ({ message }) => {
         {message.user.displayName}
       </div>
       <div style={styles.time}>
-        {message.time}
+        {moment(message.createdAt).fromNow()}
       </div>
     </div>
   );
